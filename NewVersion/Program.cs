dc5f8dd0be6f8a4c8b18c6ba99bb7584f6865745
FrederikJA <frederik.andreasen@keysight.com>
2024-03-08 09:22:43 +0100

    multiple benchmarks

--- NewVersion/Program.cs
+++ NewVersion/Program.cs
@@ -1,50 +1,86 @@
-﻿using Parquet.Schema;
-using Parquet;
-using System.Diagnostics;
+﻿using Parquet;
 using Parquet.Data;
+using Parquet.Rows;
+using Parquet.Schema;
+using System.Diagnostics;
 
-namespace NewVersion
+namespace OldVersion
 {
     internal class Program
     {
+        static StreamWriter csv;
+
+        static int file = 0;
         static void Main(string[] args)
         {
             MainAsync(args);
+            Task.Delay(1000).Wait();
             Console.ReadKey();
         }
 
-        async static void MainAsync(string[] args)
+        static async void MainAsync(string[] args)
         {
-            int rowGroups = 1_000;
-            int rowsPerGroup = 10_000;
-            DataField[] fields = new DataField[20];
+            // Warmup
+            var fs = File.OpenWrite(DateTime.Now.ToString("T") + ".results.csv");
+            csv = new StreamWriter(fs);
+            for (int i = 0; i < 10; i++)
+            {
+                await RunBenchmark<int>(1_000, 1_000, new DataField[10], false, (i, j) => i * j);
+            }
+            csv.Dispose();
+            fs.Dispose();
+            fs = File.OpenWrite(DateTime.Now.ToString("T") + ".results.csv");
+            csv = new StreamWriter(fs);
+
+            // Benchmarks
+            await RunBenchmark<int>(100, 1_000, new DataField[10], false, (i, j) => i * j);
+            await RunBenchmark<int>(1_000, 1_000, new DataField[10], false, (i, j) => i * j);
+            await RunBenchmark<int>(10_000, 1_000, new DataField[10], false, (i, j) => i * j);
+            await RunBenchmark<int>(1_000, 100, new DataField[10], false, (i, j) => i * j);
+            await RunBenchmark<int>(1_000, 1_000, new DataField[10], false, (i, j) => i * j);
+            await RunBenchmark<int>(1_000, 10_000, new DataField[10], false, (i, j) => i * j);
+            await RunBenchmark<int>(1_000, 1_000, new DataField[1], false, (i, j) => i * j);
+            await RunBenchmark<int>(1_000, 1_000, new DataField[10], false, (i, j) => i * j);
+            await RunBenchmark<int>(1_000, 1_000, new DataField[100], false, (i, j) => i * j);
+            await RunBenchmark<string>(1_000, 1_000, new DataField[10], false, (i, j) => (i * j).ToString());
+
+            csv.Dispose();
+            fs.Dispose();
+            await Console.Out.WriteLineAsync("Done");
+        }
+
+        private static async Task RunBenchmark<T>(int rowGroups, int rowsPerGroup, DataField[] fields, bool useTable, Func<int, int, T> createFunc)
+        {
+            Console.WriteLine("Generating Data");
             for (int i = 0; i < fields.Length; i++)
             {
-                fields[i] = new DataField($"Column{i}", typeof(string));
+                fields[i] = new DataField($"Column{i}", typeof(T));
             }
             ParquetSchema schema = new ParquetSchema(fields);
-            DataColumn[] columns = new DataColumn[fields.Length];
-            for (int i = 0; i < columns.Length; i++)
-            {
-                Array data = Array.CreateInstance(typeof(string), rowsPerGroup);
-                for (int j = 0; j < rowsPerGroup; j++)
-                {
-                    data.SetValue($"({i}, {j})", j);
-                }
-                columns[i] = new DataColumn(fields[i], data);
-            }
+            Table table = CreateTable(rowsPerGroup, fields, createFunc);
+            DataColumn[] columns = CreateColumns(rowsPerGroup, fields, createFunc);
 
+            Console.WriteLine("Starting writing");
             Stopwatch sw = Stopwatch.StartNew();
 
-            var stream = File.Open(DateTime.Now.ToString("T") + ".parquet", FileMode.CreateNew);
+            string path = DateTime.Now.ToString("T") + "." + file++ + ".parquet";
+            var stream = File.Open(path, FileMode.CreateNew);
             var writer = await ParquetWriter.CreateAsync(schema, stream);
             for (int i = 0; i < rowGroups; i++)
             {
-                var rowGroupWriter = writer.CreateRowGroup();
-
-                for (int j = 0; j < columns.Length; j++)
+                if (useTable)
                 {
-                    await rowGroupWriter.WriteColumnAsync(columns[j]);
+                    await writer.WriteAsync(table);
+                }
+                else
+                {
+                    var rowGroupWriter = writer.CreateRowGroup();
+
+                    for (int j = 0; j < columns.Length; j++)
+                    {
+                        await rowGroupWriter.WriteColumnAsync(columns[j]);
+                    }
+                    rowGroupWriter.Dispose();
                 }
             }
             writer.Dispose();
@@ -52,11 +88,46 @@ namespace NewVersion
             stream.Dispose();
 
             sw.Stop();
+            var fi = new FileInfo(path);
             Console.WriteLine($"{rowGroups} rowgroups of {rowsPerGroup} rows");
             Console.WriteLine($"{sw.Elapsed.ToStringFormatted()}");
+            csv.WriteLine($"{rowGroups}\t{rowsPerGroup}\t{fields.Length}\t{fi.Length}\t{typeof(T).Name}\t{sw.Elapsed.ToStringFormatted()}\t{sw.Elapsed.TotalNanoseconds}");
+        }
+
+        private static DataColumn[] CreateColumns<T>(int rowsPerGroup, DataField[] fields, Func<int, int, T> createFunc)
+        {
+            DataColumn[] columns = new DataColumn[fields.Length];
+            for (int i = 0; i < columns.Length; i++)
+            {
+                Array data = Array.CreateInstance(typeof(T), rowsPerGroup);
+                for (int j = 0; j < rowsPerGroup; j++)
+                {
+                    data.SetValue(createFunc(i + 1, j + 1), j);
+                }
+                columns[i] = new DataColumn(fields[i], data);
+            }
+
+            return columns;
+        }
+
+        private static Table CreateTable<T>(int rowsPerGroup, DataField[] fields, Func<int, int, T> createFunc)
+        {
+            Table table = new Table(fields);
+            for (int i = 0; i < rowsPerGroup; i++)
+            {
+                object[] data = new object[fields.Length];
+                for (int j = 0; j < fields.Length; j++)
+                {
+                    data.SetValue(createFunc(i + 1, j + 1), j);
+                }
+                table.Add(data);
+            }
+            return table;
         }
     }
+
 }
+
 internal static class TimespanExtensions
 {
     internal static string ToStringFormatted(this TimeSpan timeSpan)
@@ -75,7 +146,7 @@ internal static class TimespanExtensions
         }
         if (timeSpan.TotalSeconds < 1000)
         {
-            return $"{timeSpan.Seconds:0}.{timeSpan.Milliseconds:000}s";
+            return $"{timeSpan.TotalSeconds:0}.{timeSpan.Milliseconds:000}s";
         }
 
         return timeSpan.ToString("g");
--- OldVersion/Program.cs
+++ OldVersion/Program.cs
@@ -1,43 +1,77 @@
 ﻿using Parquet;
 using Parquet.Data;
+using Parquet.Data.Rows;
 using System.Diagnostics;
+using System.IO;
 
 namespace OldVersion
 {
     internal class Program
     {
+        static StreamWriter csv;
+
+        static int file = 0;
         static void Main(string[] args)
         {
-            int rowGroups = 10_000;
-            int rowsPerGroup = 1_000;
-            DataField[] fields = new DataField[3];
+            // Warmup
+            var fs = File.OpenWrite(DateTime.Now.ToString("T") + ".results.csv");
+            csv = new StreamWriter(fs);
+            for (int i = 0; i < 10; i++)
+            {
+                RunBenchmark<int>(1_000, 1_000, new DataField[10], false, (i, j) => i * j);
+            }
+            fs.Dispose();
+            fs = File.OpenWrite(DateTime.Now.ToString("T") + ".results.csv");
+            csv = new StreamWriter(fs);
+
+            // Benchmarks
+            RunBenchmark<int>(100, 1_000, new DataField[10], false, (i, j) => i * j);
+            RunBenchmark<int>(1_000, 1_000, new DataField[10], false, (i, j) => i * j);
+            RunBenchmark<int>(10_000, 1_000, new DataField[10], false, (i, j) => i * j);
+            RunBenchmark<int>(1_000, 100, new DataField[10], false, (i, j) => i * j);
+            RunBenchmark<int>(1_000, 1_000, new DataField[10], false, (i, j) => i * j);
+            RunBenchmark<int>(1_000, 10_000, new DataField[10], false, (i, j) => i * j);
+            RunBenchmark<int>(1_000, 1_000, new DataField[1], false, (i, j) => i * j);
+            RunBenchmark<int>(1_000, 1_000, new DataField[10], false, (i, j) => i * j);
+            RunBenchmark<int>(1_000, 1_000, new DataField[100], false, (i, j) => i * j);
+            RunBenchmark<string>(1_000, 1_000, new DataField[10], false, (i, j) => (i * j).ToString());
+
+            csv.Dispose();
+            fs.Dispose();
+        }
+
+        private static void RunBenchmark<T>(int rowGroups, int rowsPerGroup, DataField[] fields, bool useTable, Func<int, int, T> createFunc)
+        {
+            Console.WriteLine("Generating Data");
             for (int i = 0; i < fields.Length; i++)
             {
-                fields[i] = new DataField($"Column{i}", typeof(int));
+                fields[i] = new DataField($"Column{i}", typeof(T));
             }
-            DataColumn[] columns = new DataColumn[fields.Length];
-            for (int i = 0; i < columns.Length; i++)
-            {
-                Array data = Array.CreateInstance(typeof(int), rowsPerGroup);
-                for (int j = 0; j < rowsPerGroup; j++)
-                {
-                    data.SetValue(i * j, j);
-                }
-                columns[i] = new DataColumn(fields[i], data);
-            }
+            Schema schema = new Schema(fields);
+            Table table = CreateTable(rowsPerGroup, fields, createFunc);
+            DataColumn[] columns = CreateColumns(rowsPerGroup, fields, createFunc);
 
+            Console.WriteLine("Starting writing");
             Stopwatch sw = Stopwatch.StartNew();
 
-            Schema schema = new Schema(fields);
-            var stream = File.Open(DateTime.Now.ToString("T") + ".parquet", FileMode.CreateNew);
+            string path = DateTime.Now.ToString("T") + "." + file++ + ".parquet";
+            var stream = File.Open(path, FileMode.CreateNew);
             var writer = new ParquetWriter(schema, stream);
             for (int i = 0; i < rowGroups; i++)
             {
-                var rowGroupWriter = writer.CreateRowGroup();
-
-                for (int j = 0;j < columns.Length; j++)
+                if (useTable)
                 {
-                    rowGroupWriter.WriteColumn(columns[j]);
+                    writer.Write(table);
+                }
+                else
+                {
+                    var rowGroupWriter = writer.CreateRowGroup();
+
+                    for (int j = 0; j < columns.Length; j++)
+                    {
+                        rowGroupWriter.WriteColumn(columns[j]);
+                    }
+                    rowGroupWriter.Dispose();
                 }
             }
             writer.Dispose();
@@ -45,11 +79,46 @@ namespace OldVersion
             stream.Dispose();
 
             sw.Stop();
-            Console.WriteLine($"{rowGroups} rowgroups of {rowsPerGroup} rows");
+            var fi = new FileInfo(path);
+            Console.WriteLine($"{rowGroups} rowgroups of {rowsPerGroup} rows. {fi.Length.ToString("N")}");
             Console.WriteLine($"{sw.Elapsed.ToStringFormatted()}");
+            csv.WriteLine($"{rowGroups}\t{rowsPerGroup}\t{fields.Length}\t{fi.Length}\t{typeof(T).Name}\t{sw.Elapsed.ToStringFormatted()}\t{sw.Elapsed.TotalNanoseconds}");
+        }
+
+        private static DataColumn[] CreateColumns<T>(int rowsPerGroup, DataField[] fields, Func<int, int, T> createFunc)
+        {
+            DataColumn[] columns = new DataColumn[fields.Length];
+            for (int i = 0; i < columns.Length; i++)
+            {
+                Array data = Array.CreateInstance(typeof(T), rowsPerGroup);
+                for (int j = 0; j < rowsPerGroup; j++)
+                {
+                    data.SetValue(createFunc(i + 1, j + 1), j);
+                }
+                columns[i] = new DataColumn(fields[i], data);
+            }
+
+            return columns;
+        }
+
+        private static Table CreateTable<T>(int rowsPerGroup, DataField[] fields, Func<int, int, T> createFunc)
+        {
+            Table table = new Table(fields);
+            for (int i = 0; i < rowsPerGroup; i++)
+            {
+                object[] data = new object[fields.Length];
+                for (int j = 0; j < fields.Length; j++)
+                {
+                    data.SetValue(createFunc(i + 1, j + 1), j);
+                }
+                table.Add(data);
+            }
+            return table;
         }
     }
+
 }
+
 internal static class TimespanExtensions
 {
     internal static string ToStringFormatted(this TimeSpan timeSpan)
@@ -68,7 +137,7 @@ internal static class TimespanExtensions
         }
         if (timeSpan.TotalSeconds < 1000)
         {
-            return $"{timeSpan.Seconds:0}.{timeSpan.Milliseconds:000}s";
+            return $"{timeSpan.TotalSeconds:0}.{timeSpan.Milliseconds:000}s";
         }
 
         return timeSpan.ToString("g");
